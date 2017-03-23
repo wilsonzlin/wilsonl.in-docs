2cc2c4a7c62bf5b2746a68a915e972253d8c915a
Wilson Lin <contact@wilsonl.in>
2017-03-23 15:25:06 +1300

    Started major refactoring to use pre-rendered, noscript-optimised app

--- compile.js
+++ compile.js
@@ -1,65 +1,241 @@
-const babel = require('babel-core');
-const fs = require('fs-extra');
+"use strict";
 
-const SRC_DIR = __dirname + '/src';
+const fs = require('fs-extra');
+const marked = require('marked');
+
+function parseMarkdown(mdText) {
+    var renderer = new marked.Renderer();
+
+    renderer.code = (code, language) => {
+        var html;
+
+        if (language && language.indexOf('x-wldoc-') == 0) {
+            switch (language.slice(8)) {
+                case 'typedline':
+                    html = parseTypedCodeLine(code);
+                    break;
+            }
+        } else if (language) {
+            html = hljs.highlight(language, code, true).value;
+        } else {
+            html = escapeHTML(code);
+        }
+
+        return `<pre>${ html }</pre>`;
+    };
+
+    renderer.paragraph = (text) => marked(text).slice(3, -5); // Remove <p> wrapping
+
+    return `<div>${ marked(mdText, { renderer: renderer }) }</div>`;
+}
+
+function parseTypedCodeLine(codeText) {
+    return codeText.replace(
+        /([| ])((?:[A-Z][a-z0-9_]+)+|zQuery|function|int|float|number|string|bool|object|array)/g,
+        (_, charBefore, type) => `${charBefore}<span class="type">${ type }</span>`
+    );
+}
+
+function sortIdFiles(a, b) {
+    let idA = Number.parseInt(a.slice(0, a.indexOf('.')), 10);
+    let idB = Number.parseInt(b.slice(0, b.indexOf('.')), 10);
+
+    return idA < idB ? -1 : idA > idB ? 1 : 0;
+}
+
+function escapeHTML(str) {
+    let entityMap = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': '&quot;',
+        "'": '&#39;',
+        "/": '&#x2F;'
+    };
+    return ("" + str).replace(/[&<>"'\/]/g, entity => entityMap[entity]);
+}
+
+const HeaderListing = function (name) {
+    return `
+        <li><a href="../${ escapeHTML(name) }">${ escapeHTML(name) }</a></li>
+    `;
+}
+
+const PaneTocCategoryEntry = function (id, name, description) {
+    return `
+        <li class="toc-category-entry" title="${ escapeHTML(description) }">
+            <label>
+                <input type="radio" name="toc-category-entry-active">
+                <a href="${ escapeHTML(id) }.html" target="article">${ escapeHTML(name) }</a>
+            </label>
+        </li>
+    `;
+};
+
+const PaneTocCategory = function (name, entries) {
+    return `
+        <div class="toc-category">
+            <dt class="toc-category-label" domonclick="this.attributes.expanded = !this.attributes.expanded">${ escapeHTML(name) }</dt>
+            <dd class="toc-category-entries-container">
+                <ul class="toc-category-entries">
+                    ${ entries.map(e => PaneTocCategoryEntry(e.id, e.name, e.description)).join("") }
+                </ul>
+            </dd>
+        </div>
+    `;
+};
+
+const ReferenceArticleSignature = function (codeHtml) {
+    return `
+        <pre class="signature">${ codeHtml }</pre>
+    `;
+};
+
+const ReferenceArticleArgument = function (name, descriptionHtml) {
+    return `
+        <div>
+            <dt class="argument-name">${ escapeHTML(name) }</dt>
+            <dd>
+                <p class="argument-description">${ descriptionHtml }</p>
+            </dd>
+        </div>
+    `;
+};
+
+const ReferenceArgumentReturn = function (valueHtml) {
+    return `
+        <li>${ valueHtml }</li>
+    `;
+};
+
+const ReferenceArticle = function (category, name, versions, description, signatures, args, returns) {
+    return `
+        <link rel="stylesheet" href="../_common/article.css">
+        <header>
+            <div class="category">${ escapeHTML(category) }</div>
+            <h1>${ escapeHTML(name) }</h1>
+            <div class="versions">${ escapeHTML(versions) }</div>
+        </header>
+
+        <section class="section-synopsis">
+            <h2>Synopsis</h2>
+            <p class="description">${ escapeHTML(description) }</p>
+            ${ signatures.map(s => ReferenceArticleSignature(s.html)).join("") }
+        </section>
+
+        <section>
+            <h2>Arguments</h2>
+            <dl class="arguments-list">
+                ${ args.map(a => ReferenceArticleArgument(a.name, a.html)).join("") }
+            </dl>
+        </section>
+
+        <section>
+            <h2>Returns</h2>
+            <ul class="returns-list">
+                ${ returns.map(r => ReferenceArgumentReturn(r.html)).join("") }
+            </ul>
+        </section>
+    `;
+};
+
+const ContentArticle = function (category, name, versions, contentHtml) {
+    return `
+        <header>
+            <div class="category">${ escapeHTML(category) }</div>
+            <h1>${ escapeHTML(name) }</h1>
+            <div class="versions">${ escapeHTML(versions) }</div>
+        </header>
+
+        <section>${ contentHtml }</section>
+    `;
+};
 
 fs.removeSync(__dirname + '/dist');
 
-// const JS_DOC_FOLDERS = ['OOML'];
-const JS_DOC_FOLDERS = [];
+const JS_DOC_FOLDERS = ['zQuery'];
+const LISTINGS_HTML = JS_DOC_FOLDERS.map(f => HeaderListing(f)).join("");
 
-JS_DOC_FOLDERS.forEach(dir => {
-	dir = `${SRC_DIR}/${dir}`;
+let generatedHtmlFiles = [];
 
-	let js = fs.readFileSync(`${dir}/doc.js`, 'utf8');
-	js = js.replace(/<ZC-IMPORT\[(.*?)\]>/g, (_, subfile) => {
-		let content = fs.readFileSync(`${dir}/doc.${subfile}.js`);
-		return content;
-	});
+JS_DOC_FOLDERS.forEach(listing => {
+    let categories = Function('"use strict"; return ' + fs.readFileSync(__dirname + '/src/' + listing + '/__metadata__.js'))();
+    let categoriesHtml = [];
+    let articleAutoIncrement = 0;
 
-	let json = Function('"use strict";return ' + js)();
-	json = JSON.stringify(json);
+    categories.forEach(category => {
+        let categoryName = category.name;
 
-	fs.writeFileSync(`${dir}/doc.json`, json);
+        let categoryEntries = category.entries.map(entry => {
+            let path = `${__dirname}/src/${listing}/${categoryName}/${entry}.md`;
+            let articleHtml;
+            let articleId = articleAutoIncrement++;
+            let ret;
+
+            let stats = fs.lstatSync(path);
+            if (stats.isDirectory()) {
+                let versions = fs.readFileSync(path + '/versions.txt');
+                let description = fs.readFileSync(path + '/description.txt');
+
+                let signatures = fs.readdirSync(path + '/signatures').filter(p => /\.txt$/.test(p)).sort(sortIdFiles).map(f => {
+                    let code = fs.readFileSync(path + '/signatures/' + f, 'utf8');
+
+                    return { html: parseTypedCodeLine(code) };
+                });
+
+                let args = fs.readdirSync(path + '/arguments').filter(p => /\.md/.test(p)).sort(sortIdFiles).map(f => {
+                    let name = f.slice(f.indexOf('.') + 1, f.lastIndexOf('.'));
+                    let markdown = fs.readFileSync(path + '/arguments/' + f, 'utf8');
+
+                    return { name: name, html: marked(markdown) };
+                });
+
+                let returns = fs.readdirSync(path + '/returns').filter(p => /\.md/.test(p)).sort(sortIdFiles).map(f => {
+                    let markdown = fs.readFileSync(path + '/returns/' + f, 'utf8');
+
+                    return { html: marked(markdown) };
+                });
+
+                articleHtml = ReferenceArticle(categoryName, entry, versions, description, signatures, args, returns);
+
+                ret = { id: articleId, name: entry, description: description };
+            } else {
+                let contentHtml = marked(fs.readFileSync(path, 'utf8'));
+
+                articleHtml = ContentArticle(categoryName, entry, "", contentHtml);
+
+                ret = { id: articleId, name: entry, description: entry };
+            }
+
+            fs.writeFileSync(__dirname + '/src/' + listing + '/' + articleId + '.html', articleHtml);
+            generatedHtmlFiles.push(listing + '/' + articleId + '.html');
+            return ret;
+        });
+
+        categoriesHtml.push(PaneTocCategory(categoryName, categoryEntries));
+    });
+
+    fs.writeFileSync(__dirname + '/src/' + listing + '/index.html', `
+        <ZC-SET[viewportTitle][${listing}]>
+        <ZC-SET[headerListings][${LISTINGS_HTML}]>
+        <ZC-SET[tocCategories][${categoriesHtml}]>
+        <ZC-IMPORT[../__zc_common__/index.html]>
+    `);
+    generatedHtmlFiles.push(listing + '/index.html');
 });
 
 require('zcompile')({
-	source: SRC_DIR,
-	destination: __dirname + '/dist',
+    source: __dirname + '/src',
+    destination: __dirname + '/dist',
 
-	minifySelectors: false,
-	onloadjs: function(code) {
-		return babel.transform(code, { presets: ['latest'] }).code;
-	},
-	files: [
-		'lib/fetch-polyfill.js', // Needs minification
-
-		'index.html',
-		'index.css',
-
-		'app/app.css',
-		'app/app.js',
-		'app/app.html',
-
-		'zQuery/index.html',
-		'zQuery/doc.json',
-
-		'zVex/index.html',
-
-		// 'OOML/index.html',
-		// 'OOML/doc.json',
-	],
-
-	copy: [
-		'lib/ooml.js',
-		'lib/babel-polyfill.js',
-		'lib/marked.js',
-		'lib/highlight.js/highlight.pack.js',
-		'lib/highlight.js/styles/atom-one-light.css',
-		'images/search.png',
-	],
+    minifySelectors: false,
+    files: [
+        'index.html',
+        '_common/app.css',
+        '_common/app.noscript.css',
+        '_common/app.js',
+        '_common/article.css',
+    ].concat(generatedHtmlFiles),
 });
 
-JS_DOC_FOLDERS.forEach(dir => {
-	fs.unlinkSync(`${SRC_DIR}/${dir}/doc.json`);
-});
+generatedHtmlFiles.forEach(p => fs.removeSync(`${__dirname}/src/${p}`));
---
+++ src/OOML/__metadata__.js
@@ -0,0 +1,101 @@
+[
+    {
+        "name": "Introduction",
+        "entries": [
+            "Welcome", // 1) How to use guide. 2) Providing feedback. 3) Required knowledge (HTML, JS, OO)
+            "About OOML",
+            "Compatibility and support", // 1) Browser support. 2) Reporting bugs and ideas. 3) Webpack, browserify, require, babel (no need). 4) Building from source.
+            "Core principles",
+            "Short overview",
+            "Environment setup and usage",
+        ],
+    },
+    {
+        "name": "Fundamentals",
+        "entries": [
+            "Namespaces",
+            "Classes",
+            "Properties",
+            "Attributes",
+            "Methods",
+            "Events",
+            "DOM",
+        ],
+    },
+    {
+        "name": "Classes",
+        "entries": [
+            "Inheritance",
+            "Abstract classes",
+            "DOM extension",
+            "Serialisation and unserialisation",
+            "Constructors",
+            "Events",
+        ],
+    },
+    {
+        "name": "Properties",
+        "entries": [
+            "Supression",
+            "Typing",
+            "Getters and setters",
+            "Default property values",
+        ],
+    },
+    {
+        "name": "Attributes",
+        "entries": [
+        ],
+    },
+    {
+        "name": "Methods",
+        "entries": [
+        ],
+    },
+    {
+        "name": "Events",
+        "entries": [
+        ],
+    },
+    {
+        "name": "DOM",
+        "entries": [
+            "Text substitution",
+            "Element substitution",
+            "Array substitution",
+            "Event listeners",
+            "Table tags",
+            "HTML attributes",
+            "OOML attributes",
+        ],
+    },
+    {
+        "name": "Type system",
+        "entries": [
+            "Primitives",
+            "Method arguments",
+            "Undefined",
+            "Objects and arrays",
+            "Numbers",
+        ],
+    },
+    {
+        "name": "Additional information",
+        "entries": [
+            "Older versions",
+            "Exceptions",
+            "Identifiers", // Recommendations and restrictions on names for attributes, properties, methods, and classes
+            "Abstractions", // Topics: 1) Don't be stupid. 2) Don't try to circumvent OOML. 3) Things that might trip you up (e.g. setTimeout on attrs). 4) There's probably a way to do it -- it's your fault.
+            "Pitfalls", // 1) Single-line comments in OOML methods will break the method if the HTML is minified. 2) Escape HTML special chars in default values, inline code and method functions.
+        ]
+    },
+    {
+        "name": "Reference",
+        "entries": [
+            "OOML Namespace",
+            "OOML Element",
+            "OOML Array",
+            "HTML syntax", // Every single special HTML tag, attribute or textContent
+        ],
+    },
+]
