2114307770510d5d8edb7f8d83cbf8df2cc34bbf
Wilson Lin <contact@wilsonl.in>
2017-08-06 13:30:18 +1200

    Updated URL generation

--- compiling/Utils/createURLPathComponent.js
+++ compiling/Utils/createURLPathComponent.js
@@ -1,11 +1,14 @@
 "use strict";
 
 const createURLPathComponent = path => {
-    path = path.replace(/[`~!@#\$%\^&\*\(\)\+=\{\}\[\]\|\\:;'"<>\?,\.\/]/g, '');
+    // Allowed: $ .
+    path = path.replace(/[`~!@#\$%\^&\*\(\)\+=\{\}\[\]\|\\:;'"<>\?,\/]/g, '');
     path = path.replace(/ /g, '-');
     // Replace contiguous hyphens with single one
     path = path.replace(/-+/g, '-');
-    path = path.toLocaleLowerCase();
+    // Removing starting or trailing hyphens
+    path = path.replace(/^-+|-+$/g, '');
+    // path = path.toLocaleLowerCase();
     path = encodeURIComponent(path);
 
     return path;
