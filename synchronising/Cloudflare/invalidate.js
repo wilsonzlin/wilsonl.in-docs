d93cfcf767cd8e9517e634627ebf663a978f7bab
Wilson Lin <contact@wilsonl.in>
2018-03-23 23:59:42 +1300

    Improve reliability of Cloudflare API

--- synchronising/Cloudflare/invalidate.js
+++ synchronising/Cloudflare/invalidate.js
@@ -12,39 +12,52 @@ const invalidate = (...keys) => {
   keys.forEach(key => assertValidKey(key));
 
   return new Promise((resolve, reject) => {
-    request({
-      method: 'DELETE',
-      url: `https://api.cloudflare.com/client/v4/zones/${APP_RESOURCES_INFO.CLOUDFLARE_ZONE_ID}/purge_cache`,
-      headers: {
-        'X-Auth-Email': CREDENTIALS.CLOUDFLARE_EMAIL,
-        'X-Auth-Key': CREDENTIALS.CLOUDFLARE_GLOBAL_API_KEY,
-      },
-      body: {
-        files: keys.map(key => `${APP_RESOURCES_INFO.CLOUDFLARE_SITE}${key}`),
-      },
-      json: true,
-    }, (err, res, body) => {
-      if (err) {
-        reject(err);
-        return;
-      }
+    let operations = [];
+    do {
+      operations.push(keys.splice(0, 500));
+    } while (keys.length);
 
-      if (body.errors) {
-        body.errors.forEach(err => console.error(`Error ${err.code}: ${err.message}`));
-      }
+    (function process(subkeys) {
+      request({
+        method: 'DELETE',
+        url: `https://api.cloudflare.com/client/v4/zones/${APP_RESOURCES_INFO.CLOUDFLARE_ZONE_ID}/purge_cache`,
+        headers: {
+          'X-Auth-Email': CREDENTIALS.CLOUDFLARE_EMAIL,
+          'X-Auth-Key': CREDENTIALS.CLOUDFLARE_GLOBAL_API_KEY,
+        },
+        body: {
+          files: subkeys.map(key => `${APP_RESOURCES_INFO.CLOUDFLARE_SITE}${key}`),
+        },
+        json: true,
+      }, (err, res, body) => {
+        if (err) {
+          reject(err);
+          return;
+        }
 
-      if (body.messages) {
-        body.messages.forEach(msg => console.info(msg));
-      }
+        if (body.errors) {
+          body.errors.forEach(err => console.error(`Error ${err.code}: ${err.message}`));
+        }
 
-      if (body.success) {
-        keys.forEach(key => {
-          console.log(`Invalidated ${key}`);
-        });
-      } else {
-        reject(new Error('Cloudflare API call not successful'));
-      }
-    });
+        if (body.messages) {
+          body.messages.forEach(msg => console.info(msg));
+        }
+
+        if (body.success) {
+          subkeys.forEach(key => {
+            console.log(`Invalidated ${key}`);
+          });
+
+          if (operations.length) {
+            process(operations.shift());
+          } else {
+            resolve();
+          }
+        } else {
+          reject(new Error('Cloudflare API call not successful'));
+        }
+      });
+    })(operations.shift());
   });
 };
 
