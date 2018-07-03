d69301b8c0882e4dc6932328eeb158650f700bc5
Wilson Lin <contact@wilsonl.in>
2018-07-03 17:52:21 +1200

    Restructure docs to support latest referenza; add static server; remove zc; add nicehash docs; prepare zQuery for new signatures format; move landing page to archives

--- compiling/compile.js
+++ compiling/compile.js
@@ -1,8 +1,11 @@
 "use strict";
 
 const referenza = require("referenza");
+const minimist = require("minimist");
 
-const FLAG_CLEAN = process.argv.slice(2).includes("clean");
+const args = minimist(process.argv.slice(2));
+
+const FLAG_CLEAN = args.clean;
 
 // All directory paths should have a trailing slash
 const PROJECT_DIR = __dirname + "/../";
@@ -12,7 +15,7 @@ const OUTPUT_DIR = PROJECT_DIR + "dist/";
 
 const STATE_PATH = PROJECT_DIR + "compiling/state.json";
 
-const PROJECT_NAMES = ["ooml", "nanoscript", "zQuery", "zc"];
+const PROJECT_NAMES = ["nanoscript", "zQuery", "nicehash"];
 const METADATA_FILE_NAME = "__metadata__.js";
 
 // URL path prefix should not have trailing slash,
@@ -30,7 +33,19 @@ referenza.compile({
 
   metadataFileName: METADATA_FILE_NAME,
 
-  projectNames: PROJECT_NAMES,
+  feedback: {
+    type: referenza.resources.feedback.type.GITHUB,
+    repoOwner: "wilsonzlin",
+    repoName: "wilsonl.in-docs",
+  },
 
-  urlPathPrefix: URL_PATH_PREFIX,
-});
+  logo: "w.l.",
+
+  projects: PROJECT_NAMES,
+
+  prefix: URL_PATH_PREFIX,
+})
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
---
+++ src/nicehash/1.0/__metadata__.js
@@ -0,0 +1,29 @@
+module.exports = {
+  categories: [
+    {
+      name: "Set",
+      entries: [
+        "Set",
+        "nh_set_TYPE_add",
+        "nh_set_TYPE_clear",
+        "nh_set_TYPE_create",
+        "nh_set_TYPE_delete",
+        "nh_set_TYPE_destroy",
+        "nh_set_TYPE_has",
+      ],
+    },
+    {
+      name: "Map",
+      entries: [
+        "Map",
+        "nh_map_TYPE_clear",
+        "nh_map_TYPE_create",
+        "nh_map_TYPE_delete",
+        "nh_map_TYPE_destroy",
+        "nh_map_TYPE_get",
+        "nh_map_TYPE_has",
+        "nh_map_TYPE_set",
+      ],
+    },
+  ]
+};
